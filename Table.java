2fa6880160ef9069a7f0a92eb1dc6f9fe86be0df
Vish katyan <82721870+vishkatyan@users.noreply.github.com>
2022-11-28 18:58:45 +0530

    Create Table.java

---
+++ Table.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Table {
+    public static void main(String[] args) {
+        System.out.println("Enter a number to print its Table from 1 to 10");
+        Scanner scr = new Scanner(System.in);
+        int a = scr.nextInt();
+        for(int i = 1; i<=10; i++){
+            System.out.println(a +"*" +i +"=" +a*i);
+        }
+    }
+}
